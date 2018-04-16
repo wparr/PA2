c9f835dd9fe9b1992fd55821131871853df3f877
Ryan Peterson <rjp@iastate.edu>
2018-04-15 23:24:40 -0500

    updated project directory

---
+++ PA2/src/pa2/NetworkInfluence.java
@@ -0,0 +1,97 @@
+package pa2;
+
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add member fields and additional methods)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may only include libraries of the form java.*)
+
+/**
+* @author Hugh Potter
+*/
+
+import java.util.ArrayList;
+
+public class NetworkInfluence
+{
+	// NOTE: graphData is an absolute file path that contains graph data, NOT the raw graph data itself
+	public NetworkInfluence(String graphData)
+	{
+		// implementation
+	}
+
+	public int outDegree(String v)
+	{
+		// implementation
+
+		// replace this:
+		return -1;
+	}
+
+	public ArrayList<String> shortestPath(String u, String v)
+	{
+		// implementation
+
+		// replace this:
+		return null;
+	}
+
+	public int distance(String u, String v)
+	{
+		// implementation:
+
+		// replace this:
+		return -1;
+	}
+
+	public int distance(ArrayList<String> s, String v)
+	{
+		// implementation
+
+		// replace this:
+		return -1;
+	}
+
+	public float influence(String u)
+	{
+		// implementation
+
+		// replace this:
+		return -1f;
+	}
+
+	public float influence(ArrayList<String> s)
+	{
+		// implementation
+
+		// replace this:
+		return -1f;
+	}
+
+	public ArrayList<String> mostInfluentialDegree(int k)
+	{
+		// implementation
+
+		// replace this:
+		return null;
+	}
+
+	public ArrayList<String> mostInfluentialModular(int k)
+	{
+		// implementation
+
+		// replace this:
+		return null;
+	}
+
+	public ArrayList<String> mostInfluentialSubModular(int k)
+	{
+		// implementation
+
+		// replace this:
+		return null;
+	}
+}
---
+++ PA2/src/pa2/WikiCrawler.java
@@ -0,0 +1,35 @@
+package pa2;
+
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add member fields and additional methods)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may only include libraries of the form java.*)
+
+/**
+* @author Hugh Potter
+*/
+
+import java.util.ArrayList;
+
+public class WikiCrawler
+{
+	static final String BASE_URL = "https://en.wikipedia.org/";
+	String seedUrl;
+	int max;
+	ArrayList<String> topics;
+	String fileName;
+
+	public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName)
+	{
+		// implementation
+	}
+
+	public void crawl()
+	{
+		// implementation
+	}
+}
